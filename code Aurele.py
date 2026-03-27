06f0fc92afe4a1395d6083954f1c3dc1a797190b
Aurele1 <aurele.rougon@gmail.com>
2026-03-27 16:40:55 +0100

    Création scoring

--- code Aurele.py
+++ code Aurele.py
@@ -1,1 +1,25 @@
-def 
+def score (copie):
+    for "médical" in copie: 
+        a=copie["medical"]
+    for "technical_issue" in copie:
+        b=copie["technical_issue"]
+    for "diplomatic_level" in copie:
+        c=copie["diplomatic_level"]
+    for "fuel" in copie:
+        d=copie["fuel"]
+
+        "scoring" = a+b+c+d
+    return "scoring"
+
+
+    
+
+
+
+
+
+
+
+
+        
+
